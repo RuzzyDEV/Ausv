0f7ae24606bbcdf313b12aa334be77451eac3dca
RuzzyDEV <jonataspix2@gmail.com>
2026-01-04 02:40:04 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Orcamento from "./pages/Orcamento";
+import PoliticaGestao from "./pages/PoliticaGestao";
+import CodigoConduta from "./pages/CodigoConduta";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/orcamento" element={<Orcamento />} />
+          <Route path="/politica-de-gestao" element={<PoliticaGestao />} />
+          <Route path="/codigo-de-conduta" element={<CodigoConduta />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
